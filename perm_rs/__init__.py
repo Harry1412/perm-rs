53f8b1530cebafe1e9bc04c35a57dee6c16910e1
Harry1412 <harrybromley@btinternet.com>
2026-04-01 21:44:19 +0100

    Ruff formatting pass

--- perm_rs/__init__.py
+++ perm_rs/__init__.py
@@ -1,6 +1,5 @@
 from .perm import permanent
 
-
-__doc__ = perm_rs.__doc__
+__doc__ = perm_rs.__doc__  # noqa: F821, RUF067
 
 __all__ = ["permanent"]
--- perm_rs/perm.py
+++ perm_rs/perm.py
@@ -1,19 +1,28 @@
+from typing import TypeVar
+
+import numpy as np
+import numpy.typing as npt
+
 from .perm_rs import (
-    _permanent_f32,
-    _permanent_f64,
     _permanent_cf32,
     _permanent_cf64,
+    _permanent_f32,
+    _permanent_f64,
 )
-import numpy as np
 
-def permanent(matrix):
-    if isinstance(matrix, np.ndarray):
-        if matrix.dtype == np.float32:
-            return _permanent_f32(matrix)
-        if matrix.dtype == np.float64:
-            return _permanent_f64(matrix)
-        if matrix.dtype == np.complex64:
-            return _permanent_cf32(matrix)
-        else:
-            return _permanent_cf64(matrix)
-    return _permanent_f64(matrix)
+T = TypeVar("T", bound=np.float32 | np.float64 | np.complex64 | np.complex128)
+
+
+def permanent(matrix: npt.NDArray[T]) -> T:
+    """
+    Calculates the permanent for a provided matrix.
+    """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Provided matrix should be a numpy array.")
+    if matrix.dtype == np.float32:
+        return _permanent_f32(matrix)
+    if matrix.dtype == np.float64:
+        return _permanent_f64(matrix)
+    if matrix.dtype == np.complex64:
+        return _permanent_cf32(matrix)
+    return _permanent_cf64(matrix)
