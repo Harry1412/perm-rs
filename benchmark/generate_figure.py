7e257e63ee88f1850ebbf84f3847ae901452adbd
Harry1412 <harrybromley@btinternet.com>
2026-04-05 20:52:00 +0100

    Updated figure generation code

--- benchmark/generate_figure.py
+++ benchmark/generate_figure.py
@@ -1,51 +1,58 @@
+from collections.abc import Callable
+from functools import partial
 from pathlib import Path
 from time import perf_counter
 
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+import thewalrus
 from scipy.stats import unitary_group
-from thewalrus import perm
 
 import perm_rs
 
 random_unitary = unitary_group.rvs
 
 
-def perm_rs_time(matrix: npt.NDArray[np.complex128]) -> float:
+def time_permanent(
+    func: Callable[[npt.NDArray[np.complex128]], complex],
+    matrix: npt.NDArray[np.complex128],
+) -> float:
     t0 = perf_counter()
-    perm_rs.permanent(matrix)
+    func(matrix)
     return perf_counter() - t0
 
 
-def perm_rs_single_time(matrix: npt.NDArray[np.complex128]) -> float:
-    t0 = perf_counter()
-    perm_rs.permanent_single(matrix)
-    return perf_counter() - t0
-
-
-def perm_rs_multi_time(matrix: npt.NDArray[np.complex128]) -> float:
-    t0 = perf_counter()
-    perm_rs.permanent_multi(matrix)
-    return perf_counter() - t0
-
-
-def thewalrus_time(matrix: npt.NDArray[np.complex128]) -> float:
-    t0 = perf_counter()
-    perm(matrix)
-    return perf_counter() - t0
+def n_best_runs(
+    func: Callable[[npt.NDArray[np.complex128]], complex],
+    size: int,
+    n_reps: int,
+    n_best: int,
+) -> list[float]:
+    sub_times = [
+        time_permanent(func, random_unitary(size, random_state=i))
+        for i in range(n_reps)
+    ]
+    return np.mean(sorted(sub_times)[:n_best])
 
 
 if __name__ == "__main__":
     # Warm up
     m = random_unitary(10)
-    perm_rs_time(m)
-    thewalrus_time(m)
+    for func in [
+        perm_rs.permanent,
+        perm_rs.permanent_single,
+        perm_rs.permanent_multi,
+        thewalrus.perm,
+    ]:
+        time_permanent(func, m)
 
     # Settings
-    n_range = range(2, 27)  # Unitary sizes
-    n_reps = 20  # Number of repeats
-    best_n = 5  # Best n times to take
+    n_range = range(2, 19)  # Unitary sizes
+    n_reps = 50  # Number of repeats
+    n_best = 5  # Best n times to take
+
+    bench_func = partial(n_best_runs, n_reps=n_reps, n_best=n_best)
 
     # Generate times
     perm_rs_t = []
@@ -54,29 +61,28 @@ if __name__ == "__main__":
     walrus_t = []
     for n in n_range:
         print(f"n = {n}")  # noqa: T201
-        mat = random_unitary(n)
 
         # perm rs optimal
-        sub_times = [perm_rs_time(mat) for _ in range(n_reps)]
-        perm_rs_t.append(np.mean(sorted(sub_times)[:best_n]))
+        perm_rs_t.append(bench_func(perm_rs.permanent, n))
 
         # perm rs single-threaded
-        sub_times = [perm_rs_single_time(mat) for _ in range(n_reps)]
-        perm_rs_single_t.append(np.mean(sorted(sub_times)[:best_n]))
+        perm_rs_single_t.append(bench_func(perm_rs.permanent_single, n))
 
         # perm rs multi-threaded
-        sub_times = [perm_rs_multi_time(mat) for _ in range(n_reps)]
-        perm_rs_multi_t.append(np.mean(sorted(sub_times)[:best_n]))
+        perm_rs_multi_t.append(bench_func(perm_rs.permanent_multi, n))
 
         # thewalurs
-        sub_times = [thewalrus_time(mat) for _ in range(n_reps)]
-        walrus_t.append(np.mean(sorted(sub_times)[:best_n]))
+        walrus_t.append(bench_func(thewalrus.perm, n))
 
     path = Path(__file__).parent
 
-    plt.plot(n_range, perm_rs_t, label="perm_rs opt")
-    plt.plot(n_range, perm_rs_single_t, label="perm_rs single")
-    plt.plot(n_range, perm_rs_multi_t, label="perm_rs multi")
+    plt.plot(n_range, perm_rs_t, label="perm_rs optimal")
+    plt.plot(
+        n_range, perm_rs_single_t, label="perm_rs single", linestyle="dotted"
+    )
+    plt.plot(
+        n_range, perm_rs_multi_t, label="perm_rs multi", linestyle="dotted"
+    )
     plt.plot(n_range, walrus_t, label="thewalrus")
     plt.xlabel("n")
     plt.ylabel("Time (seconds)")
@@ -89,21 +95,23 @@ if __name__ == "__main__":
     plt.plot(
         n_range,
         np.array(perm_rs_t) / np.array(walrus_t) * 100,
-        label="opt",
+        label="optimal",
     )
     plt.plot(
         n_range,
         np.array(perm_rs_single_t) / np.array(walrus_t) * 100,
         label="single-threaded",
+        linestyle="dotted",
     )
     plt.plot(
         n_range,
         np.array(perm_rs_multi_t) / np.array(walrus_t) * 100,
         label="multi-threaded",
+        linestyle="dotted",
     )
     plt.xlabel("n")
     plt.ylabel("Relative calculation time (%)")
-    plt.axhline(100, color="black", linestyle="--")
+    plt.axhline(100, color="black", linestyle="--", label="thewalrus")
     plt.title("Relative calculation time of perm_rs to thewalrus.")
     plt.ylim(0, 400)
     plt.legend()
