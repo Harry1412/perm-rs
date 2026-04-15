0721723ea09e64c434c0fe0930ea5e1470e38116
Harry1412 <harrybromley@btinternet.com>
2026-04-15 08:29:47 +0100

    Remove unnecessary crate when calling permanent functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use num_complex::Complex;
 use numpy::PyReadonlyArray2;
+use pyo3::exceptions;
 use pyo3::prelude::*;
 
 mod permanent;
@@ -11,50 +12,50 @@ pub use profiler::performance_profiler;
 
 #[pyfunction]
 fn _permanent_single_f32<'py>(matrix: PyReadonlyArray2<'py, f32>) -> PyResult<f32> {
-    Ok(crate::permanent_single(matrix.as_array()))
+    Ok(permanent_single(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_single_f64<'py>(matrix: PyReadonlyArray2<'py, f64>) -> PyResult<f64> {
-    Ok(crate::permanent_single(matrix.as_array()))
+    Ok(permanent_single(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_single_cf32<'py>(
     matrix: PyReadonlyArray2<'py, Complex<f32>>,
 ) -> PyResult<Complex<f32>> {
-    Ok(crate::permanent_single(matrix.as_array()))
+    Ok(permanent_single(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_single_cf64<'py>(
     matrix: PyReadonlyArray2<'py, Complex<f64>>,
 ) -> PyResult<Complex<f64>> {
-    Ok(crate::permanent_single(matrix.as_array()))
+    Ok(permanent_single(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_multi_f32<'py>(matrix: PyReadonlyArray2<'py, f32>) -> PyResult<f32> {
-    Ok(crate::permanent_multi(matrix.as_array()))
+    Ok(permanent_multi(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_multi_f64<'py>(matrix: PyReadonlyArray2<'py, f64>) -> PyResult<f64> {
-    Ok(crate::permanent_multi(matrix.as_array()))
+    Ok(permanent_multi(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_multi_cf32<'py>(
     matrix: PyReadonlyArray2<'py, Complex<f32>>,
 ) -> PyResult<Complex<f32>> {
-    Ok(crate::permanent_multi(matrix.as_array()))
+    Ok(permanent_multi(matrix.as_array()))
 }
 
 #[pyfunction]
 fn _permanent_multi_cf64<'py>(
     matrix: PyReadonlyArray2<'py, Complex<f64>>,
 ) -> PyResult<Complex<f64>> {
-    Ok(crate::permanent_multi(matrix.as_array()))
+    Ok(permanent_multi(matrix.as_array()))
 }
 
 /// Runs a comparison between single & multi-threaded performance for a range of
